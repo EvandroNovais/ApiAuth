4cd4197bd486a900d236d7adcc91372ea72a26a2
Evandro Oliveira de Novais <38353553+EvandroNovais@users.noreply.github.com>
2024-04-12 09:59:21 -0300

    Update Settings.cs
    Protectioin credentials

--- Settings.cs
+++ Settings.cs
@@ -2,6 +2,6 @@ namespace ApiAuth
 {
     public static class Settings
     {
-        public static string Secret = "mj849u54kg048u50293gj849750ik935y78v";
+        public static string Secret = "${secret}";
     }
 }
